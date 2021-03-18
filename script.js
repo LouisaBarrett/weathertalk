25a3df7aee30ef417c62d31c6b9de6f04719be3c
Louisa Barrett <louisabarrett@gmail.com>
2021-03-18 17:45:10 -0600

    JS refactor to use  instead of

--- script.js
+++ script.js
@@ -6,33 +6,22 @@ checkWeatherBtn.addEventListener('click', event => {
   event.preventDefault();
   const cityNameDisplay = document.querySelector("#city-input-js");
   const cityNameRequest = cityNameInput.value;
-  cityNameDisplay.append("Checking the Weather in " + cityNameRequest);
-  updateCity(cityNameRequest)
-  dataCall();
+  cityNameDisplay.innerHTML = "Let's check the weather in " + cityNameRequest;
+  dataCall(cityNameRequest);
 });
 
 function updateCity(city) {
-  weatherData = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=f2e53f539786e6ab3e9318da74a9bc35`;
+  return `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=f2e53f539786e6ab3e9318da74a9bc35`;
 };
 
-function createListItem(text) {
-  var li = document.createElement('li');
-  li.textContent = text;
-  return li;
-};
-
-function appendChildren(parent, children) {
-  children.forEach(function (child){
-    parent.appendChild(createListItem(child));
-  });
-};
-
-function dataCall(data) {
-  console.log("dataCall weatherData", weatherData)
+function dataCall(cityName) {
+  const weatherData = updateCity(cityName)
   fetch(weatherData)
   .then(response => response.json())
   .then(data => {
-    let locationItems = [data.name, data.main.temp, data.weather[0].description]
-    appendChildren(weatherList, locationItems);
+    const string = `<li> ${data.name} </li>
+                    <li> ${data.main.temp} </li>
+                    <li> ${data.weather[0].description} </li>`;
+    weatherList.innerHTML = string;
   });
 };
