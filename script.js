e0f06460210d948159207c0503dd2cbb1a734ae6
Louisa Barrett <louisabarrett@gmail.com>
2021-03-18 19:27:18 -0600

    Add details to city weather data rendered on page

--- script.js
+++ script.js
@@ -4,24 +4,25 @@ const cityNameInput = document.querySelector('#city');
 
 checkWeatherBtn.addEventListener('click', event => {
   event.preventDefault();
-  const cityNameDisplay = document.querySelector("#city-input-js");
-  const cityNameRequest = cityNameInput.value;
-  cityNameDisplay.innerHTML = "Let's check the weather in " + cityNameRequest;
-  dataCall(cityNameRequest);
+  getData(cityNameInput.value);
 });
 
 function updateCity(city) {
   return `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=f2e53f539786e6ab3e9318da74a9bc35`;
 };
 
-function dataCall(cityName) {
+function getData(cityName) {
   const weatherData = updateCity(cityName)
   fetch(weatherData)
   .then(response => response.json())
   .then(data => {
-    const string = `<li> ${data.name} </li>
-                    <li> ${data.main.temp} </li>
+    const cityNameDisplay = document.querySelector("#city-name-js");
+    cityNameDisplay.innerHTML = "Here's what the weather looks like today in " + data.name;
+    const string = `<li> Current Temperature: <strong>${Math.floor(data.main.temp)}</strong> </li>
+                    <li> Low Temperature: <strong>${Math.floor(data.main.temp_min)}</strong> </li>
+                    <li> High Temperature: <strong>${Math.floor(data.main.temp_max)}</strong> </li>
                     <li> ${data.weather[0].description} </li>`;
     weatherList.innerHTML = string;
+    cityNameInput.value = "";
   });
 };
