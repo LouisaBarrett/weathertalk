06157e8282b903fb2cea4950283b8f2615f8563b
Louisa Barrett <louisabarrett@gmail.com>
2021-03-21 14:05:44 -0600

    Add backgroundColorUpdate() to working JS file

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const submitBtn = document.querySelector('#city-btn-js')
 const nameInput = document.querySelector('#city')
 const cityNameDisplay = document.querySelector('#city-name-js')
 const phraseHelper = document.querySelector('#phrase-helper-js')
+const mainSection = document.querySelector('section')
 const API_KEY = 'f2e53f539786e6ab3e9318da74a9bc35' // Replace this key with your own unique API key
 
 submitBtn.addEventListener('click', event => {
@@ -35,11 +36,25 @@ const phraseGenerator = (data) => {
   }
 }
 
+const backgroundColorUpdate = (element, data) => {
+  if (data.main.temp >= "70") {
+    element.removeAttribute("class")
+    element.classList.add("hot-temp-bg")
+  } else if (data.main.temp <= "30") {
+    element.removeAttribute("class")
+    element.classList.add("cool-temp-bg")
+  } else {
+    element.removeAttribute("class")
+    element.classList.add("mid-temp-bg")
+  }
+}
+
 const fetchData = (city) => {
   const weatherData = updateCity(city)
   fetch(weatherData)
   .then(response => response.json())
   .then(data => {
+    backgroundColorUpdate(mainSection, data)
     phraseHelper.innerHTML = phraseGenerator(data)
     cityNameDisplay.innerHTML = generateTitle(data)
     weatherList.innerHTML = generateHTML(data)
