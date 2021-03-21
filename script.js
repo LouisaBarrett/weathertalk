959ef66edfc5fc1f922317aa6fa5699ddbd126b3
Louisa Barrett <louisabarrett@gmail.com>
2021-03-21 09:54:51 -0600

    Refactor JS into single responsibility functions

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
-const weatherList = document.querySelector('.weather-list');
-const submitBtn = document.querySelector('#city-btn-js');
-const nameInput = document.querySelector('#city');
+const weatherList = document.querySelector('.weather-list')
+const submitBtn = document.querySelector('#city-btn-js')
+const nameInput = document.querySelector('#city')
+const cityNameDisplay = document.querySelector("#city-name-js")
+const API_KEY = "f2e53f539786e6ab3e9318da74a9bc35"
 
 submitBtn.addEventListener('click', event => {
-  event.preventDefault();
-  fetchData(nameInput.value);
-});
+  event.preventDefault()
+  fetchData(nameInput.value)
+})
 
-let updateCity = (city) => {
-  return `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=f2e53f539786e6ab3e9318da74a9bc35`;
-};
+const updateCity = (city) => {
+  return `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${API_KEY}`
+}
 
-let fetchData = (cityName) => {
+const generateTitle = (data) => {
+  return `Here's what the weather looks<br> like today in ${data.name}`
+}
+
+const generateHTML = (data) => {
+  return `<li> Current Temperature: <strong>${Math.floor(data.main.temp)}</strong> </li>
+          <li> Low Temperature: <strong>${Math.floor(data.main.temp_min)}</strong> </li>
+          <li> High Temperature: <strong>${Math.floor(data.main.temp_max)}</strong> </li>
+          <li> ${data.weather[0].description} </li>`
+}
+
+const fetchData = (cityName) => {
   const weatherData = updateCity(cityName)
   fetch(weatherData)
   .then(response => response.json())
   .then(data => {
-    const cityNameDisplay = document.querySelector("#city-name-js");
-    cityNameDisplay.innerHTML = "Here's what the weather looks<br> like today in " + data.name;
-    const string = `<li> Current Temperature: <strong>${Math.floor(data.main.temp)}</strong> </li>
-                    <li> Low Temperature: <strong>${Math.floor(data.main.temp_min)}</strong> </li>
-                    <li> High Temperature: <strong>${Math.floor(data.main.temp_max)}</strong> </li>
-                    <li> ${data.weather[0].description} </li>`;
-    weatherList.innerHTML = string;
-    nameInput.value = "";
-  });
-};
+    cityNameDisplay.innerHTML = generateTitle(data)
+    weatherList.innerHTML = generateHTML(data)
+    nameInput.value = ""
+  })
+}
